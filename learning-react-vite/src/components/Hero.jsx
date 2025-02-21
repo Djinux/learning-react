39f8fb15c56e64481d5a26547039ebdf4205e2f8
Djinux <djcizmas@gmail.com>
2025-02-21 20:21:07 +0100

    Add and style Hero section

--- learning-react-vite/src/components/Hero.jsx
+++ learning-react-vite/src/components/Hero.jsx
@@ -1,7 +1,19 @@
+import '../styles/Navbar.css'
+import phoneImage from '../assets/image-mockups.png'
+import '../styles/Hero.css'
 export default function Hero () {
     return (
         <>
-        
+            <div className="hero-section">
+                <div className="hero-section-text">
+                    <h1>Next generation digital banking</h1>
+                    <p>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, investing, and much more.</p>
+                    <button className="btn-request-invite">Request Invite</button>
+                </div>
+                <div className="hero-image">
+                    <img className="phone-image" src={phoneImage} alt="Phone image" />  
+                </div> 
+            </div>
         </>
     );
 }
--- learning-react-vite/src/components/Navbar.jsx
+++ learning-react-vite/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import easyBankLogo from "../assets/logo.svg"
-import { Link } from "react-router-dom"
+import easyBankLogo from '../assets/logo.svg'
+import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 
 export default function Navbar () {
+
     return (
         <>
             <div className="nav-bar">
-                <img src={easyBankLogo} alt="Easy bank logo " />
+                <img className="nav-logo" src={easyBankLogo} alt="Easy bank logo " />
                 <div className="link-wrapper">
                     <Link className="nav-link" to="/">Home</Link>
                     <Link className="nav-link" to="/">About</Link>
