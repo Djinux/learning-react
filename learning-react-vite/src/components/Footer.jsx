e24aab17395e648415f6f9c14c629acec325e826
Djinux <djcizmas@gmail.com>
2025-02-27 15:11:21 +0100

    Refactor styles for better responsiveness

--- learning-react-vite/src/components/Footer.jsx
+++ learning-react-vite/src/components/Footer.jsx
@@ -4,23 +4,46 @@ import youtubeIcon from '../assets/icon-youtube.svg'
 import twitterIcon from '../assets/icon-twitter.svg'
 import pinterestIcon from '../assets/icon-pinterest.svg'
 import instagramIcon from '../assets/icon-instagram.svg'
+import { Link } from 'react-router-dom'
 import '../styles/Footer.css'
 
 export default function Footer () {
     return (
         <>
-            <div className="footer-d1">
-                <img className="logo" src={logoImage} alt="Easybank logo image" />
-                <div className="footer-social-network">
-                    <img className="face" src={facebookIcon} alt="Facebook icon" />
-                    <img src={youtubeIcon} alt="Youtube icon" />
-                    <img src={twitterIcon} alt="Twitter icon" />
-                    <img src={pinterestIcon} alt="Pinterest icon" />
-                    <img src={instagramIcon} alt="Instagram icon" />
+            <div className="footer-root">
+                <div className="footer-d1">
+                    <div className="footer-logo-social">
+                    <img className="footer-logo" src={logoImage} alt="Easybank logo image" />
+                    <div className="footer-social-network">
+                        <img className="face" src={facebookIcon} alt="Facebook icon" />
+                        <img src={youtubeIcon} alt="Youtube icon" />
+                        <img src={twitterIcon} alt="Twitter icon" />
+                        <img src={pinterestIcon} alt="Pinterest icon" />
+                        <img src={instagramIcon} alt="Instagram icon" />
+                    </div>
+                </div>
+                <div className="footer-nav">
+                    <Link className="footer-link">About Us</Link>
+                    <Link className="footer-link">Contact</Link>
+                    <Link className="footer-link">Blog</Link>
+                </div>
+                <div className="footer-nav">
+                    <Link className="footer-link">Careers</Link>
+                    <Link className="footer-link">Support</Link>
+                    <Link className="footer-link">Privacy Policy</Link>
                 </div>
             </div>
-            <div></div>
-            <div></div>
+
+            <div className="footer-req-invite">
+                <div className="footer-req">
+                    <button>Request Invite</button>
+                    <p>© Easybank. All Rights Reserved</p>
+                </div>
+                <div className="attribution">
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Djina Cizmas</a>.
+                </div>
+            </div>
+            </div>
         </>
     );
 }
--- learning-react-vite/src/components/Hero.jsx
+++ learning-react-vite/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import '../styles/Navbar.css'
 import phoneImage from '../assets/image-mockups.png'
+import heroImage from '../assets/bg-intro-desktop.svg'
 import '../styles/Hero.css'
 
 export default function Hero () {
@@ -12,6 +13,7 @@ export default function Hero () {
                     <button className="btn-request-invite">Request Invite</button>
                 </div>
                 <div className="hero-image">
+                    <img className="hero-background-img" src={heroImage} alt="Hero main image" />
                     <img className="phone-image" src={phoneImage} alt="Phone image" />  
                 </div> 
             </div>
