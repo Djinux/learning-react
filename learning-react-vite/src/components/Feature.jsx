fb992f144d6d706795e3d84bf76879ffcd06f9cb
Djinux <djcizmas@gmail.com>
2025-02-28 11:32:27 +0100

    Fix warning

--- learning-react-vite/src/components/Feature.jsx
+++ learning-react-vite/src/components/Feature.jsx
@@ -3,7 +3,7 @@ import '../styles/Feature.css'
 export default function Feature (props) {
     return (
         <>
-            <div className="feature" key={props.id} className="feature">
+            <div className="feature" key={props.id}>
                 <img src={props.image} alt={props.title} />
                 <h3>{props.title}</h3>
                 <p>{props.text}</p>
